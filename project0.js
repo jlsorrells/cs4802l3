6ed1a98066ecd5541c2334c0c3d8d3c36f2500f7
Jon Sorrells <jlsorrells@wpi.edu>
2015-01-20 20:32:23 -0500

    points

--- project0.js
+++ project0.js
@@ -3,16 +3,31 @@
 var svgContainer = d3.select("body").append("svg")
                                     .attr("width", 1000)
                                     .attr("height", 500);
-
-//Draw a Circle
-var circle = svgContainer.append("circle")
-                         .attr("cx", 150)
-                         .attr("cy", 150)
-                         .attr("r", 100)
-                         .attr("fill", "hsl(120,100%,50%)");
                          
 var points = [[50,50],[60,170],[150,100],[70,10],[70,30]];
 
-var poly = svgContainer.append("polygon")
+/*var poly = svgContainer.append("polygon")
                        .attr("fill", "hsl(240,100%,50%)")
-                       .attr("points", points);
+                       .attr("points", points);*/
+
+//draw some points of different sizes
+var x = 10;
+for (var i = 0; i < 18; i++) {
+    //the size of the current point
+    var radius = Math.pow(1.3, i);
+    x += radius;
+    svgContainer.append("circle").attr("cx", x)
+                                 .attr("cy", 100)
+                                 .attr("r", radius)
+                                 // give points different colors
+                                 .attr("fill", "hsl("+ (radius * 5 - 1000 / radius) +",100%,50%)");
+    //space 5 pixels between the points
+    x += 5 + radius;
+}
+
+//label for the points
+svgContainer.append("text").attr("x", 10)
+                           .attr("y", 60)
+                           .attr("font-size", "30px")
+                           .text("Points");
+                           
