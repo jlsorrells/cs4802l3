600e867737ab7a4acf2a683f1f4138383997a998
Jon Sorrells <jlsorrells@wpi.edu>
2015-01-20 19:33:00 -0500

    playing around with polygons

--- project0.js
+++ project0.js
@@ -1,11 +1,18 @@
-//some example code from https://www.dashingd3js.com/svg-basic-shapes-and-d3js
+
 //Make an SVG Container
 var svgContainer = d3.select("body").append("svg")
-                                    .attr("width", 200)
-                                    .attr("height", 200);
+                                    .attr("width", 1000)
+                                    .attr("height", 500);
 
-//Draw the Circle
+//Draw a Circle
 var circle = svgContainer.append("circle")
-                         .attr("cx", 30)
-                         .attr("cy", 30)
-                         .attr("r", 20);
+                         .attr("cx", 150)
+                         .attr("cy", 150)
+                         .attr("r", 100)
+                         .attr("fill", "hsl(120,100%,50%)");
+                         
+var points = [[50,50],[60,170],[150,100],[70,10],[70,30]];
+
+var poly = svgContainer.append("polygon")
+                       .attr("fill", "hsl(240,100%,50%)")
+                       .attr("points", points);
